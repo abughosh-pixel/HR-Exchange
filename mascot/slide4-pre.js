a38d86e1608d38da727ef46dafe02d199b969615
abughosh-pixel <abughosh12345@gmail.com>
2026-09-19 11:18:30 +0300

    Rename mascot slides as HR Exchange Topics

--- mascot/slide4-pre.js
+++ mascot/slide4-pre.js
@@ -11,7 +11,7 @@
     'Performance Management Done Right',
     'Oracle HCM Hands-On'
   ];
-  const slides = titles.map((title, index) => `<section class="slide mascot-bites-slide" data-title="${title}"><iframe src="../mascot-bites/?bite=${index}" title="Learning Bite ${index + 1}: ${title}"></iframe></section>`).join('');
+  const slides = titles.map((title, index) => `<section class="slide mascot-bites-slide" data-title="${title}"><iframe src="../mascot-bites/?bite=${index}" title="HR Exchange Topic ${index + 1}: ${title}"></iframe></section>`).join('');
   original.outerHTML = `${slides}<div id="topicGrid" hidden></div>`;
   document.head.insertAdjacentHTML('beforeend', `<style>.mascot-bites-slide{padding:0!important;overflow:hidden!important;background:#f7f4ed!important}.mascot-bites-slide iframe{display:block;width:100%;height:100dvh;border:0;background:#f7f4ed}</style>`);
   const top = document.querySelector('#top');
